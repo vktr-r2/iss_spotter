18b56e0d771da768f3425f697084c327287e7350
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-14 15:31:17 -0400

    Add functionality to iss.js > now returns IP address , call fetchMyIP in index.js successfully

---
+++ iss.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+
+    if (error) {
+      callback(error, null); // Call the callback with an error
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const IP = JSON.parse(body);   //Parse the raw data into a JS object
+    callback(null, IP);
+
+  });
+};
+
+module.exports = { fetchMyIP };
