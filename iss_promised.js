84050cffc1cb0b7a48e3ef9ee5dc236dfe771358
Viktor Ristic <vktr.r2@protonmail.com>
2023-04-15 17:18:02 -0400

    Add request functionality to iss_promised.js

--- iss_promised.js
+++ iss_promised.js
@@ -2,3 +2,10 @@ const request = require('request-promise-native');
 
 
 
+request('https://api.ipify.org?format=json')
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error)
+  })
